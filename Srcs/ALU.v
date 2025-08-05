3e69ea385673bd151e9179f601d18e29a9b880f8
KaustubhJoshi81 <75177510+KaustubhJoshi81@users.noreply.github.com>
2025-08-06 01:14:26 +0530

    Add files via upload

--- Srcs/ALU.v
+++ Srcs/ALU.v
@@ -1,5 +1,5 @@
 module ALU(
-    input  [2:0]  ALUControl,
+    input  [3:0]  ALUControl,
     input  [31:0] a,
     input  [31:0] b,
     output reg [31:0] result,
@@ -10,14 +10,17 @@ always @(*)
 begin
     case(ALUControl)
     //Arithmetic Operations
-    3'b000: result <= a+b;         //ADD Operation
-    3'b001: result <= a-b;         //Sub 
+    4'b0000: result <= a+b;         //ADD Operation
+    4'b0001: result <= a-b;         //Sub 
     
     //Logical Oprations
-    3'b010: result <= a&b;         //AND Operation
-    3'b011: result <= a|b;         //OR 
-    
-    3'b101: result <= (a<b) ? 1:0; //SLT (Set less than)
+    4'b0010: result <= a&b;         //AND Operation
+    4'b0011: result <= a|b;         //OR 
+    4'b0100: result <= a^b;         //xor
+    4'b0101: result <= (a<b) ? 1:0; //SLT (Set less than)
+    4'b0110: result <= a>>b[4:0];   //srl
+    4'b0111: result <= a>>>b[4:0];  //sra
+    4'b1000: result <= a<<b[4:0];   //sll
     
     default: result <= 32'bx;
     endcase
